b6125417d73c7a5bdb93781c523488685d5455e9
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2019-03-09 18:46:38 -0300

    Redux exciting start

--- src/components/rental/RentalDetail.js
+++ src/components/rental/RentalDetail.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react';
 export class RentalDetail extends Component {
 
 	render() {
+
 	return (
 				<div>
-					<h1>I am a rental detail component</h1>
+					<h1>I am a rental detail component {this.props.match.params.id}</h1>
 		  	</div>
 		);
 	}
--- src/components/rental/RentalList.js
+++ src/components/rental/RentalList.js
@@ -1,70 +1,11 @@
 import React , { Component } from 'react';
 import { RentalCard } from './RentalCard'
+import { connect } from 'react-redux';
 
-export class RentalList extends Component {
-
-	constructor() {
-		super();
-
-		this.state = {
-			rentals: [{
-					id: 1,
-					title: "Central Apartment",
-					city: "New York",
-					street: "Times Sqaure",
-					category: "apartment",
-					image: "http://via.placeholder.com/350x250",
-					bedrooms: 3,
-					description: "Very nice apartment",
-					dailyRate: 34,
-					shared: false,
-					createdAt: "24/12/2017"
-				},
-				{
-					id: 2,
-					title: "Central Apartment 2",
-					city: "San Francisco",
-					street: "Main street",
-					category: "condo",
-					image: "http://via.placeholder.com/350x250",
-					bedrooms: 2,
-					description: "Very nice apartment",
-					dailyRate: 12,
-					shared: true,
-					createdAt: "24/12/2017"
-				},
-				{
-					id: 3,
-					title: "Central Apartment 3",
-					city: "Bratislava",
-					street: "Hlavna",
-					category: "condo",
-					image: "http://via.placeholder.com/350x250",
-					bedrooms: 2,
-					description: "Very nice apartment",
-					dailyRate: 334,
-					shared: true,
-					createdAt: "24/12/2017"
-				},
-				{
-					id: 4,
-					title: "Central Apartment 4",
-					city: "Berlin",
-					street: "Haupt strasse",
-					category: "house",
-					image: "http://via.placeholder.com/350x250",
-					bedrooms: 9,
-					description: "Very nice apartment",
-					dailyRate: 33,
-					shared: true,
-					createdAt: "24/12/2017"
-				}
-			]
-		}
-	}
+ class RentalList extends Component {
 
 	renderRentals() {
-		return this.state.rentals.map((rental, i) => {
+		return this.props.rentals.map((rental, i) => {
 			return (
 				  <RentalCard 
 							key={i} 
@@ -75,7 +16,7 @@ export class RentalList extends Component {
 	}
 
 	addRental() {
-		const rentals = this.state.rentals;
+		const rentals = this.props.rentals;
 		rentals.push(1);
 
 		this.setState({
@@ -84,6 +25,7 @@ export class RentalList extends Component {
 	}
 
 	render () {
+	
 		return (
 			       <section id='rentalListing'>
             <h1 className='page-title'>Your Home All Around the World</h1>
@@ -95,3 +37,12 @@ export class RentalList extends Component {
 		)
 	}
 }
+
+function mapStateToProps(state) {
+
+	return {
+		rentals: state.rentals
+	}
+}
+
+export default connect(mapStateToProps)(RentalList)
