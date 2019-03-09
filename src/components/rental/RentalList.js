6a7059048a4a5c9f70b18e3fabaffe8085979803
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2019-03-09 15:16:57 -0300

    tirando o hard code

--- src/components/rental/RentalList.js
+++ src/components/rental/RentalList.js
@@ -7,14 +7,69 @@ export class RentalList extends Component {
 		super();
 
 		this.state = {
-			rentals: [1,2,3,4,5,6]
+			rentals: [{
+					id: 1,
+					title: "Central Apartment",
+					city: "New York",
+					street: "Times Sqaure",
+					category: "apartment",
+					image: "http://via.placeholder.com/350x250",
+					bedrooms: 3,
+					description: "Very nice apartment",
+					dailyRate: 34,
+					shared: false,
+					createdAt: "24/12/2017"
+				},
+				{
+					id: 2,
+					title: "Central Apartment 2",
+					city: "San Francisco",
+					street: "Main street",
+					category: "condo",
+					image: "http://via.placeholder.com/350x250",
+					bedrooms: 2,
+					description: "Very nice apartment",
+					dailyRate: 12,
+					shared: true,
+					createdAt: "24/12/2017"
+				},
+				{
+					id: 3,
+					title: "Central Apartment 3",
+					city: "Bratislava",
+					street: "Hlavna",
+					category: "condo",
+					image: "http://via.placeholder.com/350x250",
+					bedrooms: 2,
+					description: "Very nice apartment",
+					dailyRate: 334,
+					shared: true,
+					createdAt: "24/12/2017"
+				},
+				{
+					id: 4,
+					title: "Central Apartment 4",
+					city: "Berlin",
+					street: "Haupt strasse",
+					category: "house",
+					image: "http://via.placeholder.com/350x250",
+					bedrooms: 9,
+					description: "Very nice apartment",
+					dailyRate: 33,
+					shared: true,
+					createdAt: "24/12/2017"
+				}
+			]
 		}
 	}
 
 	renderRentals() {
 		return this.state.rentals.map((rental, i) => {
 			return (
-				  <RentalCard key={i} colNum='col-md-3 col-xs-6' />
+				  <RentalCard 
+							key={i} 
+							rental={rental} 
+							colNum='col-md-3 col-xs-6' />
 			)
 		})
 	}
