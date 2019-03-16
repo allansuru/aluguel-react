a4b891d8c435710390afd24512df1657338602e6
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2019-03-16 02:33:27 -0300

    ajustes finos

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Redirect } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { Header } from './shared/Header';
-import  RentalList from './components/rental/rental-listing/RentalList';
+import  RentalListing from './components/rental/rental-listing/RentalListing';
 import RentalDetail from './components/rental/rental-detail/RentalDetail';
 
 import { init } from './reducers';
@@ -26,7 +26,7 @@ class App extends Component {
         <Header />
         <div className='container'>
           <Route exact path='/' render={ () =>  <Redirect to='/rentals' /> } />
-          <Route exact path='/rentals' component={ RentalList  } />
+          <Route exact path='/rentals' component={ RentalListing  } />
           <Route exact path='/rentals/:id' component={ RentalDetail } />
           </div>
         </div>
--- src/components/rental/rental-listing/RentalList.js
+++ src/components/rental/rental-listing/RentalList.js
@@ -1,12 +1,10 @@
 import React , { Component } from 'react';
 import { RentalCard } from './RentalCard'
-import { connect } from 'react-redux';
 
-import * as action from './../../../actions';
-
- class RentalList extends Component {
+ export class RentalList extends Component {
 
 	renderRentals() {
+		debugger
 		return this.props.rentals.map((rental, i) => {
 			return (
 				  <RentalCard 
@@ -17,39 +15,12 @@ import * as action from './../../../actions';
 		})
 	}
 
-	addRental() {
-		const rentals = this.props.rentals;
-		rentals.push(1);
-
-		this.setState({
-			rentals
-		})
-	}
-
-	componentWillMount() {
-
-		this.props.dispatch(action.fetchRentals());
-	}
-
 	render () {
-	
+
 		return (
-			       <section id='rentalListing'>
-            <h1 className='page-title'>Your Home All Around the World</h1>
             <div className='row'>
 							{ this.renderRentals() }
             </div>
-						{/* <button onClick={() => { this.addRental() }}>Add Rental!</button> */}
-          </section>
 		)
 	}
 }
-
-function mapStateToProps(state) {
-
-	return {
-		rentals: state.rentals.data
-	}
-}
-
-export default connect(mapStateToProps)(RentalList)
