18169221b0543d25d336e321f6833a2414ac3e35
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2019-03-17 21:12:53 -0300

    Handle Errors

--- server/routers/rentals.js
+++ server/routers/rentals.js
@@ -11,8 +11,9 @@ Rental.find({}, (err, foundRentals) => {
 router.get('/:id', (req, res) => {
 	const rentalId = req.params.id;
 	Rental.findById(rentalId, (err, foundRental) => {
-		res.json(foundRental);
+		if (err) {res.status(422).send({errors: [{ title: 'Rental Error!', detail: 'Could not find Rental' }] }) }
+				res.json(foundRental);
 	});
-})
+});
 
 module.exports = router;
